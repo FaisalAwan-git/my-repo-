610f9783b649ffa829f78dc6720318b4e122f5dc
FaisalAwan-git <faisalmlik9211@gmail.com>
2025-05-10 13:13:34 +0500

    Create faisalinfo.cpp

---
+++ faisalinfo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Faisal Javed" << endl;
+    cout << "Roll No: 24-ST-060" << endl;
+    cout << "Department: BSc Software Engineering Technology" << endl;
+    return 0;
+}
